e20e64203a872957b56c9907dd814a85b42ba6c4
Santos Jiménez Linares <sjimenez77@gmail.com>
2014-11-18 17:51:59 +0000

    Navbar icon updated

--- src/controllers.php
+++ src/controllers.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-// -- PORTADA -----------------------------------------------------------------
+// -- PORTADA ------------------------------------------------------------------
 $app->get('/{_locale}', function (Request $request) use ($app) {
     return $app['twig']->render('content.twig', array());
 })
@@ -23,7 +23,7 @@ $app->get('/', function (Request $request) use ($app) {
 ->bind('start');
 
 // -----------------------------------------------------------------------------
-$app->post('/contacto', function (Request $request) use ($app) {
+$app->post('/contact', function (Request $request) use ($app) {
 	// Obtenemos los datos del formulario
 	$nombre = $request->get('nombre');
 	$empresa = $request->get('empresa');
@@ -52,34 +52,4 @@ $app->post('/contacto', function (Request $request) use ($app) {
 
     return $app -> redirect($app['url_generator']->generate('portada'));
 })
-->bind('contacto');
-
-// -----------------------------------------------------------------------------
-
-$app->post('/formacion', function (Request $request) use ($app) {
-	// Obtenemos los datos del formulario
-	$nombre = $request->get('inscripcion_nombre');
-	$email = $request->get('inscripcion_email');
-	$tel = $request->get('inscripcion_tel');
-	$localidad = $request->get('inscripcion_localidad');
-	
-	// Construimos el cuerpo del mensaje
-	$body = "<h1>Inscripción a jornadas de difusión</h1><hr>";
-	$body .= "Nombre y apellidos: <strong>".$nombre."</strong><br>";
-	$body .= "Email: <strong>".$email."</strong><br>";
-	$body .= "Teléfono: <strong>".$tel."</strong><hr>";
-	$body .= "Desearía inscribirme en las jornadas de difusión en <strong>".$localidad."</strong> y fecha indicados.";
-
-	$message = \Swift_Message::newInstance()
-		->setContentType('text/html')
-		->setCharset('UTF-8')
-        ->setSubject('Inscripción a jornadas de difusión')
-        ->setFrom(array('noreply@agrokaam.com'))
-        ->setTo($app['default_contact'])
-        ->setBody($body);
-
-    $app['mailer']->send($message);
-
-    return $app -> redirect($app['url_generator']->generate('portada'));
-})
-->bind('formacion');
+->bind('contact');
